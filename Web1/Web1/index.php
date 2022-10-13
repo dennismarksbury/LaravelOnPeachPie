0366d13899ec20c997d1d8a65b6fa54a4bfeb6e2
Cider Coder <dennis.marksbury@gmail.com>
2022-10-13 15:09:03 -0400

    Adding new project that partially works

---
+++ Web1/Web1/index.php
@@ -0,0 +1,82 @@
+<?php
+
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Application;
+
+define('LARAVEL_START', microtime(true));
+
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
+
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
+
+require __DIR__.'/vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
+
+//$app = __DIR__ . '/bootstrap/app.php';
+
+// **** was in bootstrap/app.php
+$t = $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__);
+$app = new Illuminate\Foundation\Application($t);
+
+$app->singleton(
+    Illuminate\Contracts\Http\Kernel::class,
+    App\Http\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Console\Kernel::class,
+    App\Console\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
+    App\Exceptions\Handler::class
+);
+
+$app->register(App\Providers\AppServiceProvider::class);
+$app->register(App\Providers\AuthServiceProvider::class);
+$app->register(App\Providers\EventServiceProvider::class);
+$app->register(App\Providers\RouteServiceProvider::class);
+$app->register(Stevebauman\Location\LocationServiceProvider::class);
+// **** was in bootstrap/app.php
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
---
+++ Web1/Web1/vendor/twig/twig/lib/Twig/NodeTraverser.php
@@ -0,0 +1,14 @@
+<?php
+/*
+use Twig\NodeTraverser;
+
+class_exists('Twig\NodeTraverser');
+
+@trigger_error('Using the "Twig_NodeTraverser" class is deprecated since Twig version 2.7, use "Twig\NodeTraverser" instead.', \E_USER_DEPRECATED);
+
+if (false) {
+    class Twig_NodeTraverser extends NodeTraverser
+    {
+    }
+}
+*/
